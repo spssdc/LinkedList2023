a82c4ecba0287317b443ca46510b1c5b1c0233c1
SDC@StPaulsSchool.org.uk <SDC@StPaulsSchool.org.uk>
2023-01-24 12:17:55 +0000

    Add project files.

---
+++ LL_Demo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LinkedList2023
+{
+    internal class LL_Demo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ILL<int> myLL = new LL<int>();
+            LL<string> newLL = new LL<string>();
+            myLL.insertFront(3);
+            myLL.insertFront(9);
+            myLL.insertFront(1);
+            myLL.traverse();
+            Console.ReadLine();
+        }
+    }
+}
